b642302ba3e8538344dca2ea0d92d04156cd5b0c
dbulkowski95 <d.bulkowski95@gmail.com>
2019-01-11 16:30:46 +0100

    Test
    Last commit did not add test files

---
+++ test/bitset.c
@@ -0,0 +1,84 @@
+/*
+ * bitset.c
+ *
+ *  Created on: Jan 7, 2019
+ *      Author: bulkodaw
+ */
+
+#include "../include/bitset.h"
+#include <limits.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void bitmap_Init(bitmapS* bitmapa, const uint32_t n)
+{
+	bitmapa->bitLen = n;
+	bitmapa->size = n / CHAR_BIT;
+	bitmapa->arr = malloc(sizeof(*bitmapa->arr) * (bitmapa->size + 1));
+	if(bitmapa->arr == NULL)
+	{
+		perror("Uninitialized array!");
+		return;
+	}
+}
+
+void bitmap_Setbit(bitmapS* bitmapa, uint32_t index, bool value)
+{
+	if(bitmapa->arr == NULL)
+	{
+		perror("Uninitialized array!");
+		return;
+	}
+	if(index >= bitmapa->bitLen)
+	{
+		perror("Index is higher than amount of bits in array");
+		return;
+	}
+	SET_BIT_VALUE(bitmapa->arr[index / CHAR_BIT], index % CHAR_BIT, value);
+}
+
+int bitmap_Getbit(bitmapS* bitmapa, uint32_t index)
+{
+	if(bitmapa->arr == NULL)
+	{
+		perror("Uninitialized array!");
+		return -1;
+	}
+	if(index >= bitmapa->bitLen)
+	{
+		perror("Index is higher than amount of bits in array");
+		return -1;
+	}
+	return GET_BIT(bitmapa->arr[index / CHAR_BIT], index % CHAR_BIT);
+}
+
+int bitmap_Clearbit(bitmapS* bitmapa, uint32_t index)
+{
+	if(index >= bitmapa->bitLen)
+	{
+		perror("Index is higher than amount of bits in array");
+		return -1;
+	}
+	return CLEAR_BIT(bitmapa->arr[index / CHAR_BIT], index % CHAR_BIT);
+}
+/* ~(~0 << n)) - create mask. Work the same way as (1 << n) -1 */
+unsigned getbits(unsigned variableX, unsigned position, unsigned ammountOfBits)
+{
+	if(ammountOfBits > position || ammountOfBits > sizeof(variableX) * CHAR_BIT)
+	{
+		perror("Beyond the limit of bits in variable");
+		return UINT32_MAX;
+	}
+	return ((variableX >> (position + 1 - ammountOfBits)) & (((unsigned)1 << ammountOfBits) - (unsigned)1));
+}
+
+unsigned setbits(unsigned variableX, unsigned position, unsigned ammountOfBits, unsigned variableY)
+{
+	if(ammountOfBits > position || ammountOfBits > sizeof(variableX) * CHAR_BIT)
+	{
+		perror("Beyond the limit of bits in variable");
+		return UINT32_MAX;
+	}
+	return (variableX & ~(~(((unsigned)1 << ammountOfBits) - (unsigned)1) << (position + (unsigned)1 - ammountOfBits))) |
+			((variableY & ~(((unsigned)1 << ammountOfBits) - (unsigned)1)) << (position + (unsigned)1 - ammountOfBits));
+}
---
+++ test/bitsetTest.c
@@ -0,0 +1,152 @@
+/*
+ * bitsetTest.c
+ *
+ *  Created on: Jan 7, 2019
+ *      Author: bulkodaw
+ */
+#include "../include/bitset.h"
+#include <criterion/criterion.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+
+Test(BIT, BIT_MOVE_BY_SHIFT)
+{
+	for (uint8_t var = 0; var < sizeof(uint32_t) * CHAR_BIT; ++var)
+	{
+		cr_expect_eq(BIT(var), pow(2,var), "This function fail.");
+	}
+}
+
+Test(BIT, GET_BIT)
+{
+	/*Create uint16_t variable */
+	uint16_t varTest = 23198;
+	/*Binary value of 23198 = 0101 1010 1001 1110*/
+	uint16_t varTestArray[sizeof(uint16_t) * CHAR_BIT];
+	varTestArray[0] = 0;
+	varTestArray[1] = 1;
+	varTestArray[2] = 1;
+	varTestArray[3] = 1;
+	varTestArray[4] = 1;
+	varTestArray[5] = 0;
+	varTestArray[6] = 0;
+	varTestArray[7] = 1;
+	varTestArray[8] = 0;
+	varTestArray[9] = 1;
+	varTestArray[10] = 0;
+	varTestArray[11] = 1;
+	varTestArray[12] = 1;
+	varTestArray[13] = 0;
+	varTestArray[14] = 1;
+	varTestArray[15] = 0;
+
+	for (uint8_t var = 0; var < sizeof(uint16_t) * CHAR_BIT; ++var)
+	{
+		cr_expect_eq(GET_BIT(varTest, var), varTestArray[var], "This function fail in %d round.", var);
+	}
+}
+
+Test(BIT, SET_BIT)
+{
+	/*Create uint16_t variable */
+	unsigned varTest = 23198;
+	/*Binary value of 23198 = 0101 1010 1001 1110*/
+
+	const unsigned expectedValueAfterSetBitArray[sizeof(uint16_t) * CHAR_BIT] = {23199, 23199, 23199,
+			23199, 23199, 23231, 23295, 23295, 23551, 23551, 24575, 24575, 24575, 32767, 32767,
+			65535};
+	for (uint8_t var = 0; var < sizeof(uint16_t) * CHAR_BIT; ++var)
+	{
+		cr_expect_eq(SET_BIT(varTest, var), expectedValueAfterSetBitArray[var], "This function fail in %d round.", var);
+	}
+}
+
+Test(BIT, CLEAR_BIT)
+{
+	unsigned varTest = 23198;
+	/*Binary value of 23198 = 0101 1010 1001 1110*/
+
+	const unsigned expectedValueAfterSetBitArray[sizeof(uint16_t) * CHAR_BIT] = {23198, 23196, 23192,
+			23184, 23168, 23168, 23168, 23040, 23040, 22528, 22528, 20480, 16384, 16384, 0,
+			0};
+	for (uint8_t var = 0; var < sizeof(uint16_t) * CHAR_BIT; ++var)
+	{
+		cr_expect_eq(CLEAR_BIT(varTest, var), expectedValueAfterSetBitArray[var], "This function fail in %d round.", var);
+	}
+}
+
+Test(BIT, SET_BIT_VALUE_0)
+{
+	unsigned varTest = 23198;
+	/*Binary value of 23198 = 0101 1010 1001 1110*/
+
+	const unsigned expectedValueAfterSetBitArray_0[sizeof(uint16_t) * CHAR_BIT] = {23198, 23196, 23192,
+			23184, 23168, 23168, 23168, 23040, 23040, 22528, 22528, 20480, 16384, 16384, 0,
+			0};
+
+	for (uint8_t var = 0; var < sizeof(uint16_t) * CHAR_BIT; ++var)
+	{
+		SET_BIT_VALUE(varTest, var, 0);
+		cr_expect_eq(varTest, expectedValueAfterSetBitArray_0[var], "SET_BIT_VALUE for 0 function fail in %d round.", var);
+	}
+
+	varTest = 23198;
+	const unsigned expectedValueAfterSetBitArray_1[sizeof(uint16_t) * CHAR_BIT] = {23199, 23199, 23199,
+			23199, 23199, 23231, 23295, 23295, 23551, 23551, 24575, 24575, 24575, 32767, 32767,
+			65535};
+	for (uint8_t var = 0; var < sizeof(uint16_t) * CHAR_BIT; ++var)
+	{
+		SET_BIT_VALUE(varTest, var, 1);
+		cr_expect_eq(varTest, expectedValueAfterSetBitArray_1[var], "SET_BIT_VALUE for 1 function fail in %d round.", var);
+	}
+}
+
+Test(BIT, BITMAP_INIT)
+{
+	bitmapS test;
+	for (uint32_t counter = 0; counter < UINT16_MAX; counter++)
+	{
+		uint32_t size = counter / CHAR_BIT;
+		bitmap_Init(&test, counter);
+		cr_expect_eq(test.bitLen, counter, "test.bitLen fail in %u round.", counter);
+		cr_expect_eq(test.size, size, "test.size fail in %u round.", counter);
+	}
+}
+
+Test(BIT, BITMAP_SETBIT)
+{
+	bitmapS test;
+	uint32_t size = 0;
+	bitmap_Init(&test, UINT16_MAX);
+	const unsigned expectedValueAfterSetBitArray[CHAR_BIT] = {1, 3, 7, 15, 31, 63, 127, 255 };
+	for (uint32_t counter = 0; counter < UINT16_MAX; counter++)
+	{
+		bitmap_Setbit(&test, counter, true);
+		if(counter % CHAR_BIT == 0 && counter != 0)
+			size++;
+		cr_expect_eq(test.arr[size], expectedValueAfterSetBitArray[counter % CHAR_BIT], "test.arr[size] fail in %u round.", counter);
+	}
+}
+
+Test(BIT, BITMAP_GETBIT)
+{
+	bitmapS test;
+	bitmap_Init(&test, UINT16_MAX);
+	for (uint32_t counter = 0; counter < UINT16_MAX; counter++)
+	{
+		if(counter & 0x01)
+			bitmap_Setbit(&test, counter, true);
+		else
+			bitmap_Setbit(&test, counter, false);
+	}
+	for (uint32_t counter = 0; counter < UINT16_MAX; counter++)
+	{
+		if(counter & 0x01)
+			cr_expect_eq(bitmap_Getbit(&test, counter), true, "test.arr[size] fail in %u round.", counter);
+		else
+			cr_expect_eq(bitmap_Getbit(&test, counter), false, "test.arr[size] fail in %u round.", counter);
+	}
+}
